c3011aad99f5798dc4b71ced78ad67bb17341379
qubvel <qubvel@gmail.com>
2020-02-19 15:53:17 +0000

    Add efficientnet weights

--- dvcapi.py
+++ dvcapi.py
@@ -2,11 +2,7 @@ import torch
 import segmentation_models_pytorch as smp
 
 
-def get_encoder_weights_path(encoder_name, encoder_weights):
-    return "weights/{}/{}.pth".format(encoder_weights, encoder_name)
-
-
-def load_weights(summon, model, weights_file_path):
+def load_weights(summon, model: torch.nn.Module, weights_file_path: str):
     summon.pull(weights_file_path)
     with open(weights_file_path, mode='rb') as fd:
         weights = torch.load(fd)
@@ -14,14 +10,14 @@ def load_weights(summon, model, weights_file_path):
     return model
 
 
-def get_model(summon, model, encoder_name="resnet34", encoder_weights="imagenet", **kwargs):
+def get_model(summon, model: str, encoder_name: str = "resnet34", encoder_weights: str = "imagenet", **kwargs):
     if model not in smp.__dict__:
         raise ValueError("No such model architecture ({}) in SMP.".format(model))
 
     model = smp.__dict__[model](encoder_name=encoder_name, encoder_weights=None, **kwargs)
 
     if encoder_weights is not None:
-        path = get_encoder_weights_path(encoder_name, encoder_weights)
+        path = "weights/{}/{}.pth".format(encoder_weights, encoder_name)
         load_weights(summon, model.encoder, path)
 
     return model
