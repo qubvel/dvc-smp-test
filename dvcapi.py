ba6b523fc3d0bf0a144b228b8684da8370ef25b9
qubvel <qubvel@gmail.com>
2020-02-19 11:27:38 +0000

    Add models weights

--- dvcapi.py
+++ dvcapi.py
@@ -1,5 +1,27 @@
 import segmentation_models_pytorch as smp
 
+
+WEIGHTS = dict(
+    resnet34="weights/resnet34-333f7ec4.pth",
+    resnet18="weights/resnet18-5c106cde.pth",
+)
+
+
+def load_weights(summon, model, weights_file):
+    summon.pull(weights_file)
+    with open(weights_file, mode='rb') as fd:
+        print("Loading weights...") 
+        weights = torch.load(fd)
+        model.load_state_dict(weights)
+    return model
+
+
 def get_model(summon, **kwargs):
     arch = kwargs.pop("arch")
-    return smp.__dict__[arch](**kwargs)
+    encoder_name = kwargs.pop("encoder_name")
+    encoder_weights = kwargs.pop("encoder_weights", None)
+    kwargs["encoder_weights"] = None
+    model = smp.__dict__[arch](**kwargs)
+    if encoder_weight is not None:
+        load_weights(summon, model.encoder, WEIGHTS[encoder_name])
+    return model
